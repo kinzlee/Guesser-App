54c24d9d96f05327b8452fc6069ce72264a138c3
kinzlee <chinemeremnwoga@gmail.com>
2020-05-31 08:45:57 +0100

    designed input field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import Card from '../components/Card';
 import Colors from '../constants/Colors';
+import Input  from '../components/Input';
 
-const StartGameScreen = ({}) => {
+const StartGameScreen = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numericInputHandler = (input) => {
+        setEnteredValue(input.replace(/[^0-9]/g, ''));
+    }
+
     return (
+        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
         <View style={styles.gameScreen}>
             <Text style={styles.title}>Start a New Game</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input 
+                style={styles.input}
+                blurOnSubmint
+                autoCaptalize="none"
+                autoCorrect={false}
+                keyboardType="number-pad"
+                maxLength={2}
+                value={enteredValue}
+                onChangeText={numericInputHandler}
+                />
                     <View style={styles.buttonContainer}>
-                        <View>
+                        <View style={styles.button}>
                         <Button title="RESET" onPress={() => {console.log('this works')}} color={Colors.primary} />
                         </View>
-                        <View>
+                        <View style={styles.button}>
                         <Button title="CONFIRM" onPress={() => {console.log('this also works')}} color={Colors.secondary} />
                         </View>
                     </View>
             </Card>
         </View>
+        </TouchableWithoutFeedback>
 
     );
 };
@@ -47,6 +65,10 @@ const styles = StyleSheet.create({
     },
     button: {
         width: 90
+    },
+    input: {
+        textAlign: 'center',
+        width: 55
     }
 });
 
